93faa1d297555ca5897cb33ffb11348d3dbc0f61
David <sebastian.830@outlook.com>
2022-06-07 08:26:17 -0500

    Datos

--- index.py
+++ index.py
@@ -4,13 +4,26 @@ from flask import Flask, render_template, request, redirect, url_for
 # Inicializar la aplicacion
 app = Flask(__name__, template_folder='templates')
 
+datosUsuario = []
 
 # Ruta principal 
-@app.route('/')
+@app.route('/' , methods=['GET','POST'])
 def home():
+    if(request.method == "POST"):
+        placa = request.form['placa']
+        fecha = request.form['fecha']
+        hora = request.form['hora'] 
+
+        if(placa == "" or fecha  == "" or hora == ""):
+            return redirect(url_for('home'))
+        else:
+            datosUsuario.append(placa, fecha, hora )
+            print(datosUsuario)
+            return redirect(url_for('home'))
+
     return render_template('home.html')
 
-    
+
 # Ejecutar la aplicacion
 if __name__ == '__main__':
     app.run(debug=True)
