ed887800317c0d2f6cef1bc50f5626e0b0024708
David <sebastian.830@outlook.com>
2022-06-07 08:51:17 -0500

    todo

--- index.py
+++ index.py
@@ -1,28 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for
-import holidayEcuador
+#import holidayEcuador
 
 # Inicializar la aplicacion
 app = Flask(__name__, template_folder='templates')
 
-datosUsuario = []
-fechasFestivas = holidayEcuador("EC-P")
+placaUsu = []
+fechaUsu = []
+horaUsu = []
+#fechasFestivas = holidayEcuador("EC-P")
 
 # Ruta principal 
 @app.route('/' , methods=['GET','POST'])
 def home():
+    
     if(request.method == "POST"):
         placa = request.form['placa']
         fecha = request.form['fecha']
         hora = request.form['hora'] 
-
         if(placa == "" or fecha  == "" or hora == ""):
             return redirect(url_for('home'))
         else:
-            datosUsuario.append(placa, fecha, hora )
-            print(datosUsuario)
+            placaUsu.append(placa)
+            fechaUsu.append(fecha)
+            horaUsu.append(hora)
             return redirect(url_for('home'))
-
+    
     return render_template('home.html')
+
 
 
 # Ejecutar la aplicacion
